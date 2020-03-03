7feba3bfbcb0920bc2c6a2ab14d7a8976041dea3
Duncan <bonsairobo@gmail.com>
2020-03-02 21:22:32 -0800

    export useful types from lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -9,5 +9,8 @@ const FRAME_SIZE: usize = 512;
 
 type AudioFrame = [f32; FRAME_SIZE];
 
-pub use midi::list_midi_input_ports;
+pub use audio_device::AudioOutputDeviceStream;
+pub use midi::{list_midi_input_ports, MidiInputDeviceStream, MidiTrackInputStream, RawMidiMessage};
 pub use instrument::Instrument;
+pub use recording::RecordingOutputStream;
+pub use synthesizer::Synthesizer;
